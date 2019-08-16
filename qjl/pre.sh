01ff6acfa06387431da1a36dd6092c3cf0aaef06
qijiale76 <171850589@smail.nju.edu.cn>
2019-08-16 11:20:16 +0800

    uniq

--- qjl/pre.sh
+++ qjl/pre.sh
@@ -1,1 +1,11 @@
-python3 data_pre.py ../data/neg_ceshi_0626length_morethan_25000.csv ../data/neg_25000.txt
+python2 data_pre.py ../data/neg_ceshi_0626length_morethan_25000.csv ../data/neg_25000.txt
+
+cp  ../data/neg_25000.txt ../data/neg_25000_bak.txt
+
+file='../data/neg_25000_bak.txt'
+
+sort -n $file | uniq
+
+sort -n $file | awk '{if($0!=line)print; line=$0}'
+
+sort -n $file | sed '$!N; /^\(.*\)\n\1$/!P; D'
