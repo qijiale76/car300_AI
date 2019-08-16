6442558e52ba7e222502dce8a85a64543ba3b44e
owhileo <773746226@qq.com>
2019-08-16 11:26:07 +0800

    pre_process

---
+++ pre_process.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+# Author: owhileo
+# Date: 2019-8-16
+# Version: 1.0
+
+def pre_process(x):
+    x=re.sub(r"[0-9a-zA-Z\- \*]+(?!柱)", "", str(x))
+    x=re.sub(r"\s+|/|\*|■|▲|-|★|！|!|℃|=|●|\?|￥|\%|'|>|<", "", (x))#|[e-zE-Z0-9]
+    x=re.sub(r"\.+|·",".", (x))
+    x=re.sub(r";|；|：|@|\$|\#", ":", (x))
+    x=re.sub(r'、|，',',',x)
+    x=re.sub(r'\[|（|【','(',x)
+    x=re.sub(r'\]|）|】',')',x)
+    x=re.sub(r'\((,|\.)\)','',x)
+    x=re.sub(r'(,\.)|(\.,)',',',x)
+    x=re.sub(r',+',',',x)
+    x=re.sub(r'(,|\.)\)',')',x)
+    x=re.sub(r'\(\)','',x)
+    x=re.sub(r"(,:)|(\.:)|(:\.)", ":", (x))
+    x=re.sub(r":+", ":", (x))
+    return x
+    
