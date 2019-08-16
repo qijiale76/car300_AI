72e00853edda0304a08e42a1b45be172e0bdf167
qijiale76 <171850589@smail.nju.edu.cn>
2019-08-16 15:29:08 +0800

    add

--- qjl/pre.sh
+++ qjl/pre.sh
@@ -4,8 +4,10 @@ cp  ../data/neg_25000.txt ../data/neg_25000_bak.txt
 
 file='../data/neg_25000_bak.txt'
 
-sort -n $file | uniq
+sort -n $file | uniq >../data/neg_25000_bak_1.txt
 
-sort -n $file | awk '{if($0!=line)print; line=$0}'
+sort -n ../data/neg_25000_bak.txt | awk '{if($0!=line)print; line=$0}'  > ../data/neg_25000_bak_2.txt
 
-sort -n $file | sed '$!N; /^\(.*\)\n\1$/!P; D'
+sort -n ../data/neg_25000_bak_2.txt | sed '$!N; /^\(.*\)\n\1$/!P; D' > ../data/neg_25000_bak_3.txt
+
+python3 data_pre_json.py  ../data/data_all.json  ../data/data_pos.txt  ../data/data_neg.txt
