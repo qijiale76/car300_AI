48e8b33232ef4f70facaefaa8dfe2579e65ddc9c
OwenLee <502576738@q.com>
2019-08-16 15:02:52 +0800

    add

--- pre_process.py
+++ pre_process.py
@@ -2,6 +2,7 @@
 # Author: owhileo
 # Date: 2019-8-16
 # Version: 1.0
+import re
 
 def pre_process(x):
     x=re.sub(r"[0-9a-zA-Z\- \*]+(?!柱)", "", str(x))
@@ -18,5 +19,8 @@ def pre_process(x):
     x=re.sub(r'\(\)','',x)
     x=re.sub(r"(,:)|(\.:)|(:\.)", ":", (x))
     x=re.sub(r":+", ":", (x))
+    x=re.sub(r'_+','',x)
+    x=re.sub(r'①|②|③|④|⑤|⑥|⑦|⑧|⑨|⑩|⑾|☆|¤|�|&','',x)
+    
     return x
     
